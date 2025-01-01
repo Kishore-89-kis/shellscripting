4493ca2216bb309d032d0d2a0aa9d95ef1bff2aa
kishore pulapathuri <pkishore255@gmail.com>
2025-01-01 15:47:07 +0530

    username check

---
+++ 04.variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "please enter your username"
+read -s USERNAME # username is the variable name
+echo "username entered: $USERNAME"
+echo "please enter your password"
+read -s paddword 
